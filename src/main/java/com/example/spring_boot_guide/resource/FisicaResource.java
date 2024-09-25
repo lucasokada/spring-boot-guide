1c02c672d14be195fcb6848ecf0252a4117b776c
Lucas Okada <leiki@ciandt.com>
2024-09-24 23:08:37 -0300

    implementa entity mapper

--- src/main/java/com/example/spring_boot_guide/resource/FisicaResource.java
+++ src/main/java/com/example/spring_boot_guide/resource/FisicaResource.java
@@ -3,12 +3,14 @@ package com.example.spring_boot_guide.resource;
 import com.example.spring_boot_guide.dto.FisicaDTO;
 import com.example.spring_boot_guide.dto.assembler.FisicaAssembler;
 import com.example.spring_boot_guide.entity.Fisica;
+import com.example.spring_boot_guide.entity.mapper.FisicaMapper;
 import com.example.spring_boot_guide.service.FisicaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +39,29 @@ public class FisicaResource {
         }
 
         return insert;
+    }
+
+    //    • PUT , que permite atualizar um recurso existente por completo. Por exemplo, a API permite
+    //    atualizar todos os campos da entidade no banco de dados; e
+    //    • PATCH , que permite atualizar parcialmente um recurso existente. Por exemplo, somente
+    //    parte dos dados de uma entidade são atualizados no banco de dados e os demais permanecem
+    //    inalterados.
+
+    @PutMapping("/")
+    public boolean update(@RequestBody FisicaDTO fisicaDto) {
+        boolean update = false;
+
+        Fisica newFisica = FisicaAssembler.dtoToEntityModel(fisicaDto);
+        Fisica fisicaUpdate = fisicaService.findByCpf(newFisica.getCpf());
+
+        FisicaMapper.update(fisicaUpdate, newFisica);
+
+        Fisica fisicaUpdated = fisicaService.update(fisicaUpdate);
+        if (fisicaUpdated != null) {
+            update = true;
+        }
+
+        return update;
     }
 
     @GetMapping("/{cpf}")
